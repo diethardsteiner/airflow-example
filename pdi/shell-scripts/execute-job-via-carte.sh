181e7eba531de775994f4ad1e5f97fe0fa1fae14
Diethard Steiner <diethard.steiner@gmail.com>
2020-04-03 17:55:10 +0100

    several bug fixes

--- pdi/shell-scripts/execute-job-via-carte.sh
+++ pdi/shell-scripts/execute-job-via-carte.sh
@@ -3,7 +3,7 @@
 # vars that should be passed as parameter to this script at some point
 CARTE_USER=cluster
 CARTE_PASSWORD=cluster
-CARTE_HOSTNAME=localhost
+CARTE_HOSTNAME=host.docker.internal
 CARTE_PORT=8081
 PDI_JOB_PATH=/Users/diethardsteiner/git/airflow-example/pdi/jobs-and-transformations/job-write-to-log.kjb
 PDI_LOG_LEVEL=Basic
@@ -15,16 +15,16 @@ set PDI_JOB_STATUS
 CARTE_SERVER_URL=http://${CARTE_USER}:${CARTE_PASSWORD}@${CARTE_HOSTNAME}:${CARTE_PORT}
 
 # start PDI job and get job id
-PDI_JOB_ID=$(curl -s "${CARTE_SERVER_URL}/kettle/executeJob/?job=${PDI_JOB_PATH}&level=${PDI_LOG_LEVEL}" | xml sel -t -m '/webresult/id' -v . -n)
+PDI_JOB_ID=$(curl -s "${CARTE_SERVER_URL}/kettle/executeJob/?job=${PDI_JOB_PATH}&level=${PDI_LOG_LEVEL}" | xmlstarlet sel -t -m '/webresult/id' -v . -n)
 
 echo "The PDI job ID is: " ${PDI_JOB_ID}
 
 function getPDIJobStatus {
-  curl -s "${CARTE_SERVER_URL}/kettle/jobStatus/?name=job-write-to-log&id=${PDI_JOB_ID}&xml=Y" | xml sel -t -m '/jobstatus/status_desc' -v . -n
+  curl -s "${CARTE_SERVER_URL}/kettle/jobStatus/?name=job-write-to-log&id=${PDI_JOB_ID}&xml=Y" | xmlstarlet sel -t -m '/jobstatus/status_desc' -v . -n
 }
 
 function getPDIJobFullLog {
-  curl -s "${CARTE_SERVER_URL}/kettle/jobStatus/?name=job-write-to-log&id=${PDI_JOB_ID}&xml=Y" | xml sel -t -m 'jobstatus/result/log_text' -v . -n
+  curl -s "${CARTE_SERVER_URL}/kettle/jobStatus/?name=job-write-to-log&id=${PDI_JOB_ID}&xml=Y" | xmlstarlet sel -t -m 'jobstatus/result/log_text' -v . -n | fold -w
 }
 
 PDI_JOB_STATUS=$(getPDIJobStatus)
@@ -44,6 +44,3 @@ echo "The PDI job status is: " ${PDI_JOB_STATUS}
 echo "Printing full log ..."
 echo ""
 echo $(getPDIJobFullLog)
-
-
-
